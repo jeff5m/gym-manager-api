d2b8754bcc013329784e5184b8cbf5aac706cbcf
jeff5m <jeffmatheuspereira@gmail.com>
2021-03-20 16:54:25 -0300

    Initial commit - config pom and docker

---
+++ src/main/java/com/gymmanager/GymManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gymmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymManagerApplication.class, args);
+    }
+
+}
