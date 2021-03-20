5e5071d21b35a5f7664cb96bcbd60c57f405b7ed
jeff5m <jeffmatheuspereira@gmail.com>
2021-03-20 19:16:52 -0300

    create custom BadRequestException

---
+++ src/main/java/com/gymmanager/api/exptionhandler/BadRequestExceptionDetails.java
@@ -0,0 +1,10 @@
+package com.gymmanager.api.exptionhandler;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class BadRequestExceptionDetails extends ExceptionDetails {
+
+}
