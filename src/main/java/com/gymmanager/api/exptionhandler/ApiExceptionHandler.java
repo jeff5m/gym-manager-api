21e21aa21e9407ce454a5470af93890e157176cc
jeff5m <jeffmatheuspereira@gmail.com>
2021-03-21 18:14:23 -0300

    override internal, arguments not valid exception handler

--- src/main/java/com/gymmanager/api/exptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/gymmanager/api/exptionhandler/ApiExceptionHandler.java
@@ -1,21 +1,30 @@
 package com.gymmanager.api.exptionhandler;
 
+import com.gymmanager.api.exptionhandler.exceptions.ExceptionDetails;
 import com.gymmanager.api.exptionhandler.exceptions.NotFoundException;
 import com.gymmanager.api.exptionhandler.exceptions.NotFoundExceptionDetails;
+import com.gymmanager.api.exptionhandler.exceptions.ValidationExceptionDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<NotFoundExceptionDetails> handlerNotFoundException(NotFoundException notFoundException) {
+    public ResponseEntity<NotFoundExceptionDetails> handleNotFoundException(NotFoundException notFoundException) {
         return new ResponseEntity<>(
                 NotFoundExceptionDetails.builder()
                         .status(HttpStatus.NOT_FOUND.value())
@@ -25,5 +34,40 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                         .developerMessage("The resource doesn't exist")
                         .build(), HttpStatus.NOT_FOUND
         );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(OffsetDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Invalid Fields")
+                        .details("Check the field(s) error")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
+                        .build(), HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .timestamp(OffsetDateTime.now(ZoneOffset.UTC))
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, status);
     }
 }
