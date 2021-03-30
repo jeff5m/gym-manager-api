eb1e3841963d94dbd45ad35c71dfc969fd80a998
jeff5m <jeffmatheuspereira@gmail.com>
2021-03-30 18:54:27 -0300

    add messages to validation annotations in studentPostRequestBody

--- src/main/java/com/gymmanager/domain/mapper/requests/student/StudentPostRequestBody.java
+++ src/main/java/com/gymmanager/domain/mapper/requests/student/StudentPostRequestBody.java
@@ -23,30 +23,30 @@ public class StudentPostRequestBody {
     @NotBlank(message = "Student must have a name")
     @Size(message = "Name length must be between 0 and 100 characters", max = 100)
     private String name;
-
     @NotBlank(message = "Student must have a valid avatar URL")
     @URL
     private String avatarUrl;
-
     @NotBlank(message = "Student must have an email")
     @Size(message = "Email length must be between 0 and 100 characters", max = 60)
     @Email
     @UniqueFieldStudent(message = "There is already an student registered with this email")
     private String email;
-
     @NotEmpty(message = "Student must have a cpf")
     @CPF(message = "The 'cpf' field must contains 11 numbers")
     @UniqueFieldStudent(message = "There is already an student registered with this cpf")
     private String cpf;
-
-    @NotNull
+    @NotNull(message = "Student must have a weight")
+    @Positive(message = "Weight must be a positive number")
     private BigDecimal weight;
-    @NotNull
+    @NotNull(message = "Student must have a height")
+    @Positive(message = "Height must be a positive number")
     private BigDecimal height;
-    @NotNull
+    @NotNull(message = "Student must have an age")
+    @Positive(message = "Students age must be greater then zero")
     private Integer age;
+    @NotNull(message = "Student must have a gender")
     @Enumerated(EnumType.STRING)
     private StudentGender gender;
-    @NotNull
+    @NotNull(message = "Student must have an Instructor")
     private StudentPostInstructorId instructor;
 }
